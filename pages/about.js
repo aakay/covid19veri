131aa991a87f8bea32d1ea9c10122c3c818a306e
Altug Akay <altug.akay@gmail.com>
2020-03-21 21:01:28 +0300

    Adjust about page

--- pages/about.js
+++ pages/about.js
@@ -5,6 +5,7 @@ import Layout from '../components/Layout';
 const About = () => (
   <Layout>
     <p>Tüm veriler <a href="https://twitter.com/drfahrettinkoca">https://twitter.com/drfahrettinkoca</a> hesabından alınmıştır. Tamamı resmi veridir.</p>
+    <p>Bu site, tüm verileri bir arada görebilmek konusunda kolaylık olması için yapılmıştır.</p>
   </Layout>
 );
 
