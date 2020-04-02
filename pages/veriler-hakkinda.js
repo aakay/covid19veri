c44ec56f76af7e547f6073a6063ad9888ba7ca40
Altug Akay <altug.akay@gmail.com>
2020-04-02 21:06:04 +0300

    Update data sources text

--- pages/veriler-hakkinda.js
+++ pages/veriler-hakkinda.js
@@ -5,7 +5,7 @@ import Layout from '../components/Layout';
 const About = () => (
   <Layout>
     <p>Tüm veriler <a href="https://twitter.com/drfahrettinkoca">https://twitter.com/drfahrettinkoca</a> hesabından ve <a href="https://covid19.saglik.gov.tr/">https://covid19.saglik.gov.tr/</a> adresinden alınmıştır. Tamamı resmi veridir.</p>
-    <p>Bu site, tüm verileri bir arada görebilmek konusunda kolaylık olması için yapılmıştır.</p>
+    <p>Bu site, tüm verileri bir arada görebilmek konusunda kolaylık olması için yapılmıştır. Herhangi resmi bir özelliği yoktur. Verilerin uyuşmaması durumunda (gözden kaçmış olabilir) kaynak olarak kullanılmamalıdır.</p>
   </Layout>
 );
 
