2970281b78b22f664ea7796e89203827f6eba8d6
Altug Akay <altug.akay@gmail.com>
2020-04-19 13:53:12 +0300

    Optimise disclaimer text

--- src/App.js
+++ src/App.js
@@ -53,8 +53,8 @@ function App() {
           </Row>
         </TabPane>
       </Tabs>
-      <p>Tüm veriler <a href="https://twitter.com/drfahrettinkoca">https://twitter.com/drfahrettinkoca</a> hesabından ve <a href="https://covid19.saglik.gov.tr/">https://covid19.saglik.gov.tr/</a> adresinden alınmıştır. Tamamı resmi veridir.</p>
-      <p>Bu site, tüm verileri bir arada görebilmek konusunda kolaylık olması için yapılmıştır. Herhangi resmi bir özelliği yoktur. Verilerin uyuşmaması durumunda (gözden kaçmış olabilir) kaynak olarak kullanılmamalıdır.</p>
+      <p style={{ marginTop: 15 }}>Tüm veriler <a href="https://twitter.com/drfahrettinkoca">https://twitter.com/drfahrettinkoca</a> hesabından ve <a href="https://covid19.saglik.gov.tr/">https://covid19.saglik.gov.tr/</a> adresinden alınmıştır. Tamamı resmi veridir.</p>
+      <p>Bu site, tüm verileri bir arada görebilmek konusunda kolaylık olması için hızlıca yapılmıştır. Herhangi resmi bir özelliği yoktur. Verilerin uyuşmaması durumunda (gözden kaçmış olabilir) kaynak olarak kullanılmamalıdır.</p>
     </div >
   );
 }
