6d6aa7d13630a2ef70012706e8fc98c43bfe3f46
Altug Akay <altug.akay@gmail.com>
2020-04-19 13:20:02 +0300

    add tabs for charts

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Tabs, Row, Col } from 'antd';
 
 import TotalCases from './components/TotalCases';
 import TotalLosses from './components/TotalLosses';
@@ -10,17 +11,51 @@ import LossToCaseRatio from './components/LossToCaseRatio';
 import './App.css';
 
 function App() {
+  const { TabPane } = Tabs;
+
   return (
     <div className="App">
-      <TotalCases />
-      <TotalLosses />
-      <DailyCases />
-      <DailyLosses />
-      <CaseTestRatio />
-      <LossToCaseRatio />
+      <Tabs defaultActiveKey={1} >
+        <TabPane tab="Toplam" key={1}>
+          <Row>
+            <Col xs={24} lg={12}>
+              <div><TotalCases /></div>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={24} lg={12}>
+              <div><TotalLosses /></div>
+            </Col>
+          </Row>
+        </TabPane>
+        <TabPane tab="Günlük" key={2}>
+          <Row>
+            <Col xs={24} lg={12}>
+              <div><DailyCases /></div>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={24} lg={12}>
+              <div><DailyLosses /></div>
+            </Col>
+          </Row>
+        </TabPane>
+        <TabPane tab="Oranlar" key={3}>
+          <Row>
+            <Col xs={24} lg={12}>
+              <div><CaseTestRatio /></div>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={24} lg={12}>
+              <div><LossToCaseRatio /></div>
+            </Col>
+          </Row>
+        </TabPane>
+      </Tabs>
       <p>Tüm veriler <a href="https://twitter.com/drfahrettinkoca">https://twitter.com/drfahrettinkoca</a> hesabından ve <a href="https://covid19.saglik.gov.tr/">https://covid19.saglik.gov.tr/</a> adresinden alınmıştır. Tamamı resmi veridir.</p>
       <p>Bu site, tüm verileri bir arada görebilmek konusunda kolaylık olması için yapılmıştır. Herhangi resmi bir özelliği yoktur. Verilerin uyuşmaması durumunda (gözden kaçmış olabilir) kaynak olarak kullanılmamalıdır.</p>
-    </div>
+    </div >
   );
 }
 
--- src/components/Chart.js
+++ src/components/Chart.js
@@ -7,15 +7,14 @@ export default ({ id, chartDataKey, headerText, labelFormatter, formatter, notes
   <>
     <h2 style={{ marginTop: 15 }}>{headerText}</h2>
     {notes && (<p>{notes}</p>)}
-    <ResponsiveContainer width={800} aspect={2}>
+    <ResponsiveContainer width="100%" aspect={1.75}>
       <AreaChart id={id} data={data} margin={{ top: 15, right: 20, left: 0 }}>
         <CartesianGrid strokeDasharray="3 3" />
         <Area dataKey={chartDataKey} type="monotone" stroke="#00a6a3" fill="#00a6a3" />
         <XAxis
           dataKey="date"
-          height={100}
+          height={50}
           tick={<CustomizedAxisTick />}
-          interval={1}
         />
         <YAxis />
         <Tooltip
