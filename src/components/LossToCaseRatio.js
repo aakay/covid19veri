a8a6aa24ab6ce392fbff02befa96d8d44e304d5a
Altug Akay <altug.akay@gmail.com>
2020-04-19 20:19:11 +0300

    Adjust title for loss to case ratio

--- src/components/LossToCaseRatio.js
+++ src/components/LossToCaseRatio.js
@@ -7,7 +7,7 @@ export default () => (
     id="lossToCaseRatioChart"
     chartDataKey="lossToCaseRatio"
     labelFormatter={(value) => `${value} tarihindeki toplam kayıp/vaka oranı`}
-    headerText="Günlük toplam kayıp/vaka oranı"
+    headerText="Toplam kayıp/vaka oranı"
     formatter={(value) => [getValueInPercent(value), '']}
     notes="Not: Vaka bilgisi eksikse o gün için veri göstrilmemektedir.Bu durum çok gerçekçi ve şu an mümkün olmasa da olasılığı öngörülmüştür."
   />
