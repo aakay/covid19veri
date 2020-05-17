d66181e7047b9481f6e114cf3d8f7cacbfc533cd
Altug Akay <altug.akay@gmail.com>
2020-05-17 13:45:19 +0300

    Remove unnecessary note

--- src/components/LossToCaseRatio.js
+++ src/components/LossToCaseRatio.js
@@ -9,6 +9,5 @@ export default () => (
     labelFormatter={(value) => `${value} tarihindeki toplam kayıp/vaka oranı`}
     headerText="Toplam kayıp/vaka oranı"
     formatter={(value) => [getValueInPercent(value), '']}
-    notes="Not: Vaka bilgisi eksikse o gün için veri göstrilmemektedir.Bu durum çok gerçekçi ve şu an mümkün olmasa da olasılığı öngörülmüştür."
   />
 );
