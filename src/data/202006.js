ccf414446071ae981d6c7a8e584567b0ff9c5e94
Altug Akay <altug.akay@gmail.com>
2020-06-01 22:50:42 +0300

    add data for 01.06

---
+++ src/data/202006.js
@@ -0,0 +1,11 @@
+export default [{
+  date: '01.06',
+  cases: 164769,
+  losses: 4563,
+  tests: 31525,
+  totalTests: 2070719,
+  positive: 827,
+  dailyLoss: 23,
+  recovery: 128947,
+  dailyRecovery: 974
+}];
--- src/data/index.js
+++ src/data/index.js
@@ -1,11 +1,13 @@
 import march2020 from './202003';
 import april2020 from './202004';
 import may2020 from './202005';
+import june2020 from './202005';
 
 const rawData = [
   ...march2020,
   ...april2020,
-  ...may2020
+  ...may2020,
+  ...june2020
 ];
 
 const data = rawData.map(entry => ({
