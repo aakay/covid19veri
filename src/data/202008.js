483b05e0cc281cdb0684bc5813b4563bc98d3b65
altug akay <17157374+aakay@users.noreply.github.com>
2020-08-02 19:48:07 +0300

    Data for 02.08

--- src/data/202008.js
+++ src/data/202008.js
@@ -8,4 +8,14 @@ export default [{
   dailyLoss: 19,
   recovery: 215516,
   dailyRecovery: 981
+}, {
+  date: '02.08',
+  cases: 232856,
+  losses: 5728,
+  tests: 40247,
+  totalTests: 4885916,
+  positive: 978,
+  dailyLoss: 18,
+  recovery: 216494,
+  dailyRecovery: 978
 }];
