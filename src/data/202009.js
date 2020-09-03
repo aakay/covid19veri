364076d8a4801ab1b1ff3e11ab703ebdcb02fc70
altug akay <17157374+aakay@users.noreply.github.com>
2020-09-03 10:07:54 +0300

    Create 202009.js

---
+++ src/data/202009.js
@@ -0,0 +1,21 @@
+export default [{
+  date: '01.09',
+  cases: 271705,
+  losses: 6417,
+  tests: 109443,
+  totalTests: 7247935,
+  positive: 1572,
+  dailyLoss: 47,
+  recovery: 245929,
+  dailyRecovery: 1003
+}, {
+  date: '02.09',
+  cases: 273301,
+  losses: 6462,
+  tests: 107927,
+  totalTests: 7355862,
+  positive: 1596,
+  dailyLoss: 45,
+  recovery: 246876,
+  dailyRecovery: 947
+}];
